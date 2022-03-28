248fb3114d3434f896fdf1c9d0026c8aeac35877
graphffiti <graphffiti@gmail.com>
2022-03-28 22:46:58 +0800

    Added server module with shallow implementation of its functions

--- src/main.rs
+++ src/main.rs
@@ -2,3 +2,21 @@ fn main() {
     let server = server::Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
+
+mod server {
+    pub struct Server {
+        addr: String,
+    }
+
+    impl Server {
+        pub fn new(addr: String) -> Self {
+            Server { addr: addr }
+        }
+
+        pub fn run(self) {
+            println!("Bind to a TCP Port and listen {}", self.addr);
+            println!("Accept the client's connection request");
+            println!("Read the client's message");
+        }
+    }
+}
